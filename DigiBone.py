4387f3cc62dac5ca7d67da2480261501931be0b8
Craig Hissett <CraigHissett@users.noreply.github.com>
2018-03-23 00:53:41 +0000

    Update DigiBone.py

--- DigiBone.py
+++ DigiBone.py
@@ -1,4 +1,5 @@
 from SynthClass import *
+import serial, time
 
 #Setup the Fuildsynth player, then connect to the Arduino.
 #Arduino will send comands  (based on its reads from the
@@ -6,11 +7,42 @@ from SynthClass import *
 #and sound on/off toggle).
 #Pi to process
 
-
 Trombone = TromSynth()
 
-Trombone.PlayExample()
+def serial_data(port, baudrate):
+    ser = serial.Serial(port, baudrate)
+
+    while True:
+        yield ser.readline()
+
+    ser.close()
+
+for line in serial_data('/dev/ttyACM0',9600):
+    if line == 'C':
+        Trombone.NoteTest(60)
+        break
+    elif line == 'D':
+        Trombone.NoteTest(62)
+        break
+    elif line == 'E':
+        Trombone.NoteTest(64)
+        break
+    elif line == 'F':
+        Trombone.NoteTest(65)
+        break
+    elif line == 'G':
+        Trombone.NoteTest(67)
+        break
+    elif line == 'A':
+        Trombone.NoteTest(69)
+        break
+    elif line == 'B':
+        Trombone.NoteTest(71)
+        break
+    elif line == 'PlayExample':
+        Trombone.PlayExample()
+    else:
+        print 'Not Recognised'
+
 
 Trombone.CloseSynth()
-
-
